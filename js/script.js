739ad3156ea22659e14fddb3a1f34a44efeeff62
Ysmart2303 <viniciussaraiva3004@gmail.com>
2026-03-23 13:48:44 -0300

    Media Maker 001

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    
+    alert("Media é " + ((nota1 + nota2) / 2))
+}
