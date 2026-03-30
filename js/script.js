9447441cdabf5c4bf70fc2e460cd1b7d4b0850b2
Ysmart2303 <viniciussaraiva3004@gmail.com>
2026-03-30 12:47:10 -0300

    Não funciona ainda :(

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ function calcular() {
     
     alert("Media é " + ((nota1 + nota2) / 2))
 }
+
+function somar() {
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    
+    alert("Soma é " + (nota1 + nota2))
+}
+
+function sub() {
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    
+    alert("Subtração é " + (nota1 - nota2))
+}
+
+function multi() {
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    
+    alert("Multiplicação é " + (nota1 * nota2))
+}
+
+function divi() {
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    
+    alert("Divisão é " + (nota1 / nota2))
+}
